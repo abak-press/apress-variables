7bb4996495c987b0271d3d405eae6a5227595dd5
Artem Napolskih <napolskih@gmail.com>
2015-09-23 12:51:59 +0500

    feature: replace nested variables

--- lib/apress/variables.rb
+++ lib/apress/variables.rb
@@ -4,3 +4,10 @@ require 'apress/variables/version'
 require 'apress/variables/variable'
 require 'apress/variables/list'
 require 'apress/variables/parser'
+
+module Apress
+  module Variables
+    class Error < StandardError; end
+    class UnknownVariableError < Error; end
+  end
+end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -24,29 +24,83 @@ describe Apress::Variables::Parser do
     end
   end
 
+  let(:var3) do
+    Apress::Variables::Variable.new.tap do |v|
+      v.id = :var3
+      v.source_proc = ->(params, args) { "var3_#{args.join}" }
+    end
+  end
+
   let(:list) { Apress::Variables::List.new }
   let(:company_id) { rand(234) }
-  let(:parser) { described_class.new(list) }
+  let(:options) { {} }
+  let(:parser) { described_class.new(list, options) }
   let(:args) { [1, 4, 's', 5, 6] }
 
   before do
     list.add(var0)
     list.add(var1)
     list.add(var2)
-  end
-
-  it "unknown variable is not replace" do
-    expect(parser.replace_variables("content {unknown_var} test", company_id: company_id))
-      .to eq "content {unknown_var} test"
+    list.add(var3)
   end
 
   it "several variables" do
-    expect(parser.replace_variables("content {int_variable} test {variable1} {unknown_var} a", company_id: company_id))
-      .to eq "content #{0} test a#{company_id}b {unknown_var} a"
+    expect(parser.replace_variables("content {int_variable} test {variable1} a", company_id: company_id))
+      .to eq "content #{0} test a#{company_id}b a"
   end
 
   it "variable with args" do
     expect(parser.replace_variables("content {variable_with_args(#{args.join(', ')})} test", company_id: company_id))
       .to eq "content #{company_id}#{args.join('_')} test"
+  end
+
+  it "variable with args" do
+    expect(parser.replace_variables(
+                   "content {var3({variable_with_args(#{args.join(', ')})})} test",
+                   company_id: company_id
+    ))
+    .to eq "content var3_#{company_id}#{args.join('_')} test"
+  end
+
+  it "nested variable without args" do
+    expect(parser.replace_variables("content {var3({variable1})} test", company_id: company_id))
+    .to eq "content var3_a#{company_id}b test"
+  end
+
+  it "nested variable with args" do
+    expect(parser.replace_variables(
+                   "content {var3({variable_with_args(#{args.join(', ')})}sometext)} test",
+                   company_id: company_id
+    ))
+    .to eq "content var3_#{company_id}#{args.join('_')}sometext test"
+  end
+
+  it "nested variable without args" do
+    expect(parser.replace_variables("content {var3({variable1})} test", company_id: company_id))
+    .to eq "content var3_a#{company_id}b test"
+  end
+
+  context "when unknown variable" do
+    context "when silent = true (default)" do
+      it "nothing replaced. returns the original string" do
+        expect(parser.replace_variables("content {int_variable} {unknown_var} test", company_id: company_id))
+        .to eq "content {int_variable} {unknown_var} test"
+      end
+
+      it "nothing replaced. returns the original string" do
+        expect(parser.replace_variables("content {var3({variable11})} test", company_id: company_id))
+        .to eq "content {var3({variable11})} test"
+      end
+    end
+
+    context "when silent = false" do
+      let(:options) { {silent: false} }
+
+      it "nothing replaced. raise UnknownVariableError" do
+        expect do
+          parser.replace_variables("content {int_variable} {unknown_var} test", company_id: company_id)
+        end.to raise_error Apress::Variables::UnknownVariableError
+      end
+    end
   end
 end
